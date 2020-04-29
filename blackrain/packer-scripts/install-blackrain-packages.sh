271914fcfcf194bc522116f566a5f6635a67e2ea
crouchr <richard.crouch@vonage.com>
2020-04-29 22:36:06 +0100

    Uncomment the additional packages

--- blackrain/packer-scripts/install-blackrain-packages.sh
+++ blackrain/packer-scripts/install-blackrain-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 # Packages that Blackrain depends on
 
-#sudo yum install -y p0f
-#sudo yum install -y nmap
-#sudo yum install -y argus
+sudo yum install -y p0f
+sudo yum install -y nmap
+sudo yum install -y argus
 
