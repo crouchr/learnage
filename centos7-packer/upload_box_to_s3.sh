ca93c25f6770d8b4079fc20c3c6cbd124342cb7b
crouchr <richard.crouch@vonage.com>
2020-04-30 11:04:33 +0100

    First version of upload_box_to_s3.sh added

---
+++ centos7-packer/upload_box_to_s3.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+AWS_PROFILE=$1      # e.g. developmentaws
+BOX_DIR=$2         # e.g. rch-centos7
+BOX_VERSION=$3      # e.g. 1.0.1
+BOX_NAME="$BOX_DIR".box
+echo
+echo "Upload the Vagrant box to my NVM S3 bucket"
+echo "------------------------------------------"
+echo "Note : This may take several minutes..."
+echo "AWS_PROFILE is ${AWS_PROFILE}"
+echo "BOX_DIR is ${BOX_DIR}"
+echo "BOX_VERSION is ${BOX_VERSION}"
+echo "BOX_NAME is ${BOX_NAME}"
+
+aws s3 \
+--profile=${AWS_PROFILE} \
+cp boxes/${BOX_DIR}/${BOX_VERSION}/${BOX_NAME} s3://richardcrouch/boxes/${BOX_NAME}
