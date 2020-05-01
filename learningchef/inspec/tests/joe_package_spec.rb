a5df812b712146273919a808186ffd511b6c13b2
crouchr <richard.crouch@vonage.com>
2020-05-01 15:29:44 +0100

    Modified system dir

--- learningchef/inspec/tests/joe_package_spec.rb
+++ learningchef/inspec/tests/joe_package_spec.rb
@@ -2,7 +2,7 @@ control "world-1" do                                  # A unique ID for this con
   impact 1.0                                          # Just how critical is
   title "Joe editor"                                  # Readable by a human
   desc "The joe editor should be installed"           # Optional description
-  describe package('joe8') do                          # The actual test
+  describe package('joe') do                          # The actual test
      it { should be_installed}
   end
 end
