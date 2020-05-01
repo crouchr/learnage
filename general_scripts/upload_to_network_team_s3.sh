66f7291ba78c25cb8668ddc87784d5191d8e1e08
crouchr <richard.crouch@vonage.com>
2020-05-01 17:46:43 +0100

    Added script to upload to S3

---
+++ general_scripts/upload_to_network_team_s3.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+FILE='/home/crouchr/CentOS7_v2_virtualbox.box'
+
+AWS_PROFILE=developmentaws
+S3_KEY=boxes
+
+echo
+echo "Upload a file to my NVM S3 bucket"
+echo "---------------------------------"
+echo "Note : This may take several minutes..."
+echo "AWS_PROFILE is ${AWS_PROFILE}"
+echo "File to be uploaded : ${FILE}"
+echo "Uploading to S3..."
+
+aws s3 \
+--profile=${AWS_PROFILE} \
+cp ${FILE} s3://richardcrouch/${S3_KEY}/${FILE}
