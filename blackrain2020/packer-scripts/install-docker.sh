a034a2f642b1ff4d22165b2b39c46c5b03433dfc
crouchr <richard.crouch@vonage.com>
2020-05-04 10:39:01 +0100

    Temporarily commented out docker install - unreliable download

--- blackrain2020/packer-scripts/install-docker.sh
+++ blackrain2020/packer-scripts/install-docker.sh
@@ -1,15 +1,17 @@
 #!/usr/bin/env bash
 echo "Installing Docker..."
 
+# Unreliable at moment so commented out temporarily
+
 # Install Docker from Docker CE (Stable)
-sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
-sudo yum -y install docker-ce-19.03.8
+#sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+#sudo yum -y install docker-ce-19.03.8
 
 # Start Docker
-sudo systemctl start docker
+#sudo systemctl start docker
 
 # Enable Docker
-sudo systemctl enable docker
+#sudo systemctl enable docker
 
 # Smoke Test - i.e. Packer job will fail if Docker won't basically run
-sudo docker version
+#sudo docker version
