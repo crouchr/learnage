ef639371749cec272e25641add3cf5cc0dba341d
crouchr <richard.crouch@vonage.com>
2020-05-06 21:02:57 +0100

    PS fixes

--- powershell/generate-box-metadata.ps1
+++ powershell/generate-box-metadata.ps1
@@ -13,16 +13,16 @@ $checksum = $checksum.Hash
 $BoxVersion
 $BoxDescription
 
-$version="1.2.3"
-$description="This is a useful box"
-$box_name="CenOS7_virtualbox_v$version.box"
+#$version="1.2.3"
+#$description="This is a useful box"
+$box_name="CenOS7_virtualbox_v$BoxVersion.box"
 $box_url="http://boxfilerepo.s3-eu-west-1.amazonaws.com/$box_name" 
 
 $template = Get-Content $Path -Raw
 
-$template=$template.Replace("<version>",$version)
+$template=$template.Replace("<version>",$BoxVersion)
 $template=$template.Replace("<checksum>",$checksum)
-$template=$template.Replace("<description>",$description)
+$template=$template.Replace("<description>",$BoxDescription)
 $template=$template.Replace("<box_url>",$box_url)
 
 $template
