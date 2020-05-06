627ef3b06b4e863394cab1bc566899b4235b38e0
crouchr <richard.crouch@vonage.com>
2020-05-06 21:36:01 +0100

    No run_list is triggered

--- powershell/generate-box-metadata.ps1
+++ powershell/generate-box-metadata.ps1
@@ -5,16 +5,13 @@ param(
     $BoxDescription
 )
 
-#[string]$Path = '/home/crouchr/PycharmProjects/learnage/powershell/template.txt'
-[string]$Path = 'template.txt'
+[string]$Path = 'box-metadata-template.txt'
 $checksum = Get-FileHash -Path $Path -Algorithm "SHA1"
 $checksum = $checksum.Hash
 
 $BoxVersion
 $BoxDescription
 
-#$version="1.2.3"
-#$description="This is a useful box"
 $box_name="CentOS7_virtualbox-v$BoxVersion.box"
 $box_url="http://boxfilerepo.s3-eu-west-1.amazonaws.com/$box_name" 
 
@@ -25,7 +22,9 @@ $template=$template.Replace("<checksum>",$checksum)
 $template=$template.Replace("<description>",$BoxDescription)
 $template=$template.Replace("<box_url>",$box_url)
 
-#$template
+# Dump to console
+$template
 
+# Save to file so can be sent to S3 and as an artifact
 $template | out-file -filepath metadata.json
 
