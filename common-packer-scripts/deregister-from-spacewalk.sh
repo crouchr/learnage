5224c9c1cd8a0439d78107b791a66e8cefe43390
crouchr <richard.crouch@vonage.com>
2020-05-10 20:15:21 +0100

    Cosmetics

--- common-packer-scripts/deregister-from-spacewalk.sh
+++ common-packer-scripts/deregister-from-spacewalk.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 # see https://www.redhat.com/archives/spacewalk-list/2017-June/msg00016.html
+echo "De-register from Chef"
+
 sudo systemctl disable rhnsd
 sudo systemctl disable rhnmd
 sudo sed -i 's/enabled *= *1/enabled=0/' /etc/yum/pluginconf.d/rhnplugin.conf
