cd18ff1fb3c126e0030be74cc2b161f7115f520f
crouchr <richard.crouch@vonage.com>
2020-05-10 20:17:00 +0100

    Tested de-register from Spacewalk

--- common-packer-scripts/deregister-from-spacewalk.sh
+++ common-packer-scripts/deregister-from-spacewalk.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 # see https://www.redhat.com/archives/spacewalk-list/2017-June/msg00016.html
-echo "De-register from Chef"
+echo "De-register from Spacewalk"
 
 sudo systemctl disable rhnsd
-sudo systemctl disable rhnmd
 sudo sed -i 's/enabled *= *1/enabled=0/' /etc/yum/pluginconf.d/rhnplugin.conf
