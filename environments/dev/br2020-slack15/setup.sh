7ef99545c100de4d2284bb8bb4e3e52520cdd099
crouchr <richard.crouch@vonage.com>
2020-05-10 21:21:42 +0100

    Added more to br2020-slack15

--- environments/dev/br2020-slack15/setup.sh
+++ environments/dev/br2020-slack15/setup.sh
@@ -17,30 +17,38 @@ echo $DEST_DIR_ROOT
 # Check for patch updates - slows up boot so need a way of avoiding this
 # slackpkg update etc. to go in here
 
-echo "Creating directory structure..."
+echo "Creating directory structure"
+echo "============================"
 mkdir -p $DEST_DIR_ROOT/app
 chown -R vagrant:vagrant $DEST_DIR_ROOT
 
-echo "Copy external packages..."
+mkdir -p /var/run/rchpids
+chown vagrant:vagrant /var/run/rchpids
+
+echo "Copy external packages"
+echo "======================"
 cp packages/* DEST_DIR_ROOT/packages/*
 
-echo "Copy the br2020 application..."
+echo "Copy the br2020 application"
+echo "==========================="
 cp app/*.py DEST_DIR_ROOT/app/*
 
-echo "Copy the br2020 etc configuration..."
+echo "Copy the br2020 etc configuration"
+echo "================================="
 cp etc/* DEST_DIR_ROOT/etc/*
 
-echo "Install Python dependencies..."
+echo "Install Python dependencies"
+echo "==========================="
 pip install --upgrade pip
 cp installer/REQUIREMENTS.TXT DEST_DIR_ROOT/installer/REQUIREMENTS.TXT
 cd DEST_DIR_ROOT/installer
 pip install REQUIREMENTS.TXT
 
-echo "Install packages..."
+echo "Install pre-compiled packages"
+echo "============================="
 cd ${ROOT_DIR}
 pwd
 #tree
 
 echo "Finished setup.sh OK for provisioning br2020-slack15"
 echo
-
