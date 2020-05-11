87127a335e352cfcd7de32063edd8624ffa0d4ff
crouchr <richard.crouch@vonage.com>
2020-05-11 14:14:29 +0100

    Access JSON information

--- rch-centos7/NetworksExecuteVboxPackerBuild.ps1
+++ rch-centos7/NetworksExecuteVboxPackerBuild.ps1
@@ -48,8 +48,9 @@ Write-Host "Env: Jenkins BUILD_DISPLAY_NAME : $Env:BUILD_DISPLAY_NAME"
 
 # Read ISO filename from vars file to add to metadata.json
 $JSON = Get-Content 'stack_vars/rch-centos7-vars.json' | Out-String | ConvertFrom-Json
+$JSON
 $IsoFilename = $JSON['iso_filename']
-$IsoFilename
+Write-Host "ISO : $IsoFilename"
 
 [string]$BuildDate = Get-Date -Format "dddd dd/MM/yyyy HH:mm K"
 [string]$BoxDescription = $Env:BOX_DESCRIPTION +`
