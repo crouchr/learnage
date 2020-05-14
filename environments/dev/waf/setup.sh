7424598a47ca95bba2e46beffe4af4cfa7b9ca44
crouchr <richard.crouch@vonage.com>
2020-05-14 16:02:05 +0100

    Added Papertrail logging to WAF

--- environments/dev/waf/setup.sh
+++ environments/dev/waf/setup.sh
@@ -7,7 +7,7 @@
 set -e	# bomb out if any problem
 
 echo 
-echo "Started setup.sh for provisioning this node"
+echo "[+] Started setup.sh for provisioning this node"
 
 # Check for patch updates - slows up boot so need a way of avoiding this
 yum update -y --disableplugin=fastestmirror
@@ -30,17 +30,22 @@ yum install -y mod_security mod_security_crs
 # Add jenkins into apache group so it can upload files
 usermod -a -G apache jenkins
 
-echo "Copying core (root-owned) web server configuration and content..."
+echo "[+] Copying core (root-owned) web server configuration and content..."
 cp /vagrant/apache/minimal-index.html /var/www/html/index.html
 chown apache:apache /var/www/html/index.html
 chmod 755 /var/www/html/index.html
 
-echo "Copying Apache configuration..."
+echo "[+] Copying Apache configuration..."
 cp /vagrant/apache/httpd.conf /etc/httpd/conf/
 cp /vagrant/apache/proxy.conf /etc/httpd/conf.d/
 cp /vagrant/apache/mod_security.conf /etc/httpd/conf.d/
 
-echo "Starting httpd..."
+echo "[+] Adding PaperTrail destination for rsyslog..."
+echo "# The following entry was created during box provisioning" > /etc/rsyslog.d/95-papertrail.conf
+echo "*.*          @logs2.papertrailapp.com:52491" >> /etc/rsyslog.d/95-papertrail.conf
+systemctl restart rsyslog
+
+echo "[+] Starting httpd..."
 systemctl start httpd.service
 systemctl enable httpd.service
 
