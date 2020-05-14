4f02cbbc5dd72a3bd0b321fb78b6f896a2ef470f
crouchr <richard.crouch@vonage.com>
2020-05-14 12:19:28 +0100

    Initial commit of WAF

---
+++ environments/dev/waf/setup.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+# Bring up the WAF server
+# This script is running on the VM itself
+# Files on the Host can be accessed via the /vagrant share
+
+set -e	# bomb out if any problem
+
+echo 
+echo "Started setup.sh for provisioning this node"
+
+# Check for patch updates - slows up boot so need a way of avoiding this
+yum update -y --disableplugin=fastestmirror
+systemctl restart sshd
+
+yum install -y httpd httpd-devel mod_ssl python-pip
+
+#yum -y install php php-common php-mysql php-pdo php-intl php-gd php-xml php-mbstring
+#echo "Include /vagrant/apache/*.conf" >> /etc/httpd/conf/httpd.conf
+
+# Install PIP
+pip install --upgrade pip
+pip install wheel
+
+#echo "date.timezone = Europe/London" >> /etc/php.ini
+
+# Add jenkins into apache group so it can upload files
+usermod -a -G apache jenkins
+
+echo "Copying core (root-owned) web server configuration and content..."
+cp /vagrant/apache/minimal-index.html /var/www/html/index.html
+chown apache:apache /var/www/html/index.html
+chmod 755 /var/www/html/index.html
+
+echo "Copying Apache configuration..."
+cp /vagrant/apache/minimal-httpd.conf /etc/httpd/httpd.conf
+
+echo "Starting httpd..."
+systemctl start httpd.service
+systemctl enable httpd.service
+
+echo "Finished setup.sh OK for provisioning this node"
+echo
