a51a2e006f36d1ced4b9ca88b6b5509b2e48c9ac
crouchr <richard.crouch@vonage.com>
2020-05-21 09:00:42 +0100

    Added minimal chef-solo test files to br2020 - part 2

---
+++ environments/dev/br2020/cookbooks/blackrain/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: blackrain
+# Recipe:: default
+# Author:: Richard Crouch (richard.crouch100@gmail.com)
+#
+# Copyright (C) 2020 Me, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# NOTE - STILL A WORK IN PROGRESS
+#
+# NOTES : Just adummy to test it does something
+#
+package 'python' #installed by default
+package 'python-magic' # for identifying file formats
+package 'python-dpkt' # for extracting info from pcaps
+#package 'python-mako' # for rendering html reports and web gui
+#package 'python-sqlalchemy'
+package 'python-jinja2' # necessary for web.py utility
+#package 'python-bottle'
+#package 'ssdeep'
+#package 'python-pyrex' # required for pyssdeep installation
+#package 'subversion'
+#package 'libfuzzy-dev'
+#package 'python-pymongo' # for mongodb support
+#package 'mongodb' # includes server and clients
+package 'g++'
+package 'libpcre3'
+package 'libpcre3-dev'
+package 'libcap2-bin'
+package 'git'
