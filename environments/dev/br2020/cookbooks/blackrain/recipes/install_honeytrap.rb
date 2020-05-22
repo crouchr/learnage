5c02e78500dbebb1ef839ac8b1de140bffed463d
crouchr <richard.crouch@vonage.com>
2020-05-22 15:17:18 +0100

    Added more vendor sources

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_honeytrap.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_honeytrap.rb
@@ -14,6 +14,11 @@
 #    ( )  submitMwserv
 #    ( )  submitNebula
 
+execute 'add_nogroup_honeytrap' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'groupadd nogroup'
+end
 
 execute 'get_honeytrap_source' do
     cwd '/usr/local/src'
@@ -21,24 +26,30 @@ execute 'get_honeytrap_source' do
     command 'curl -o /usr/local/src/honeytrap-master-may2020.zip http://web.ermin/br2020-packages/honeytrap-master-may2020.zip'
 end
 
-execute 'install_honeytrap' do
+execute 'unzip_honeytrap' do
+    cwd '/usr/local/src'
+    command 'unzip honeytrap-master-may2020.zip'
+    user 'root'
+end
+
+execute 'autogen_honeytrap' do
     cwd '/usr/local/src/honeytrap-master'
     command 'autoreconf -fvi'
     user 'root'
 end
 
-execute 'install_honeytrap' do
+execute 'configure_honeytrap' do
     cwd '/usr/local/src/honeytrap-master'
     command './configure --with-stream-mon=nfq --with-efence --with-logattacker --with-cspm --with-cpuemu --enable-devmodules'
     user 'root'
 end
 
-execute 'install_barnyard2' do
+execute 'make_honeytrap' do
     cwd '/usr/local/src/honeytrap'
     command 'make'
     user 'root'
 end
-execute 'install_barnyard2' do
+execute 'install_honeytrap' do
     cwd '/usr/local/src/honeytrap'
     command 'make install'
     user 'root'
