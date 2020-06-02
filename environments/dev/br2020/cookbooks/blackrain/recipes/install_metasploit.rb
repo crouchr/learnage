0be98fcc404e416a42b130a0dfd809b544c52989
crouchr <richard.crouch@vonage.com>
2020-06-02 23:48:20 +0100

    Added Zeek (bro replacement) including basic install recipe

---
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_metasploit.rb
@@ -0,0 +1,25 @@
+# Install Metasploit
+
+#curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall && \
+#  chmod 755 msfinstall && \
+#  ./msfinstall
+
+
+execute 'get_metasploit_source' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'curl https://raw.githubusercontent.com/rapid7/metasploit-omnibus/master/config/templates/metasploit-framework-wrappers/msfupdate.erb > msfinstall'
+end
+
+execute 'chmod_msfinstall' do
+    cwd '/usr/local/src'
+    command 'chmod 755 msfinstall'
+    user 'root'
+end
+
+execute 'install_metasploit' do
+    cwd '/usr/local/src'
+    command './msfinstall'
+    user 'root'
+end
+
