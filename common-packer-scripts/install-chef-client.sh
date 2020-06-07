db25eda022c176a4b6888c94aecea5625a7d4269
crouchr <richard.crouch@vonage.com>
2020-06-07 23:19:15 +0100

    Updated Chef client version to 16.1.16

--- common-packer-scripts/install-chef-client.sh
+++ common-packer-scripts/install-chef-client.sh
@@ -2,12 +2,14 @@
 # Install Chef Client
 
 USER=vagrant
+CHEF_CLIENT_VERSION=16.1.16
 
 # Install prerequisites for Chef
 sudo yum install -y git
 
 # Install Chef Client - this is latest as of April 2020
-sudo curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v 15.9.17
+#sudo curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v 15.9.17
+sudo curl -L https://www.chef.io/chef/install.sh | sudo bash -s -- -v ${CHEF_CLIENT_VERSION}
 
 sudo mkdir -p /home/${USER}/.chef
 sudo chown ${USER}:${USER} /home/${USER}/.chef
@@ -17,5 +19,3 @@ sudo chown ${USER}:${USER} /home/${USER}/chef-repo
 
 sudo mkdir -p /etc/chef
 sudo chown ${USER}:${USER} /etc/chef
-
-
