9eb187e1c5b3f66b5490322cf2de323487c3d245
crouchr <richard.crouch@vonage.com>
2020-06-07 23:44:00 +0100

    Added tcpdump and traceroute to centos7 box

--- common-packer-scripts/install-tools-packages.sh
+++ common-packer-scripts/install-tools-packages.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Core utilities
-sudo yum install -y yum-utils wget lsof net-tools htop
+sudo yum install -y yum-utils wget lsof net-tools htop tcpdump traceroute
 
 # Add packages I personally like to use
 sudo yum install -y joe shadow-utils tree ncdu
