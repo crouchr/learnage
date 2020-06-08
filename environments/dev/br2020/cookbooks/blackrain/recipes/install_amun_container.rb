f9e95d31f4ba4ab876ac65c6b614502ac6b5689a
crouchr <richard.crouch@vonage.com>
2020-06-08 13:50:12 +0100

    First version of Amun dockerised

---
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_amun_container.rb
@@ -0,0 +1,32 @@
+# Build the Glastopf 3.1.3 container
+
+directory '/usr/local/src/amun' do
+  owner 'crouchr'
+  group 'crouchr'
+  mode '0777'
+  action :create
+end
+
+directory '/usr/local/src/amun/dist_files' do
+  owner 'crouchr'
+  group 'crouchr'
+  mode '0777'
+  action :create
+end
+
+cookbook_file "/usr/local/src/amun/Dockerfile" do
+  source "amun/Dockerfile"
+  mode "0644"
+end
+
+cookbook_file "/usr/local/src/amun/dist_files/amun.conf" do
+  source "amun/dist_files/amun.conf"
+  mode "0644"
+end
+
+# Build the container
+execute 'build_amun_container' do
+    cwd '/usr/local/src/amun'
+    user 'root'
+    command 'docker build -t crouchr:amun .'
+end
