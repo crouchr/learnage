03eb1fe907cb127c6ce58890b99738eca81c121a
crouchr <richard.crouch@vonage.com>
2020-06-10 17:03:39 +0100

    Initial version of br2020.service

--- environments/dev/br2020/cookbooks/blackrain/recipes/default.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/default.rb
@@ -10,71 +10,101 @@ execute 'set_hostname' do
     command 'hostnamectl set-hostname br2020'
 end
 
+# Misc global scripts e.g. starting honeypots etc
+directory '/opt/br2020' do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
+# Misc global scripts e.g. starting honeypots etc
+directory '/opt/br2020/bin' do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
+# Global configuration
+directory '/opt/br2020/etc' do
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
 # 777 works - maybe try 744 ?
 directory '/app' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 # Store the Blackrain app
 directory '/app/src' do
-  owner 'crouchr'
-  group 'crouchr'
-  mode '0777'
-  action :create
-end
-
-# Misc scripts e.g. starting honeypots etc
-directory '/app/scripts' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 # Store the Blackrain app unit tests
 directory '/app/src/tests' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 directory '/app/src/tests/data' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 # Store the GeoIP database
 directory '/usr/local/share/GeoIP' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 # These mounts are where the logs in the docker honeypots will be sent
 directory '/data' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
+directory '/data/honeytrap' do
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 directory '/data/amun' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
 
 directory '/data/glastopf' do
-  owner 'crouchr'
-  group 'crouchr'
+  owner 'vagrant'
+  group 'vagrant'
+  mode '0777'
+  action :create
+end
+
+directory '/data/cowrie' do
+  owner 'vagrant'
+  group 'vagrant'
   mode '0777'
   action :create
 end
--- environments/dev/br2020/cookbooks/blackrain/recipes/install_scripts.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_scripts.rb
@@ -1,19 +1,19 @@
-cookbook_file "/app/scripts/start_honeypots.sh" do
+cookbook_file "/opt/br2020/bin/start_honeypots.sh" do
   source "start_honeypots.sh"
   mode "0755"
 end
 
-cookbook_file "/app/scripts/docker-compose.yml" do
-  source "docker-compose.yml"
-  mode "0644"
-end
-
-cookbook_file "/app/scripts/rules.sh" do
+cookbook_file "/opt/br2020/bin/rules.sh" do
   source "rules.sh"
   mode "0644"
 end
 
-cookbook_file "/app/scripts/br2020.service" do
+cookbook_file "/opt/br2020/etc/br2020.service" do
   source "br2020.service"
+  mode "0644"
+end
+
+cookbook_file "/opt/br2020/etc/br2020.yml" do
+  source "br2020.yml"
   mode "0644"
 end
