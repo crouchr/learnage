e09200dc632ed4b36df8418c988c2d77ab393edc
crouchr <richard.crouch@vonage.com>
2020-06-11 00:23:42 +0100

    Docker installing reliably now

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_docker.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_docker.rb
@@ -2,70 +2,10 @@
 # Unreliable at moment so commented out temporarily
 # https://download.docker.com/linux/centos/7/x86_64/stable/Packages/ - contains rpms - todo - use the rpms
 
-# execute 'get_docker-selinux_package' do
-#     cwd '/tmp'
-#     user 'root'
-#     command 'curl -o /tmp/docker-ce-selinux-17.03.3.ce-1.el7.x86_64.rpm https://download.docker.com/linux/centos/7/x86_64/stable/Packages/docker-ce-selinux-17.03.3.ce-1.el7.x86_64.rpm'
-# end
-#
-# execute 'get_containerd_package' do
-#     cwd '/tmp'
-#     user 'root'
-#     command 'curl -o /tmp/containerd.io-1.2.13-3.2.el7.x86_64.rpm http://web.ermin/br2020-packages/containerd.io-1.2.13-3.2.el7.x86_64.rpm'
-# end
-#
-# execute 'get_docker_package' do
-#     cwd '/tmp'
-#     user 'root'
-#     command 'curl -o /tmp/docker-ce-19.03.10-3.el7.x86_64.rpm http://web.ermin/br2020-packages/docker-ce-19.03.10-3.el7.x86_64.rpm'
-# end
-#
-# execute 'get_docker_cli_package' do
-#     cwd '/tmp'
-#     user 'root'
-#     command 'curl -o /tmp/docker-ce-cli-19.03.10-3.el7.x86_64.rpm http://web.ermin/br2020-packages/docker-ce-cli-19.03.10-3.el7.x86_64.rpm'
-# end
-#
-# rpm_package 'docker-ce-selinux' do
-#     action :install
-#     source '/tmp/docker-ce-selinux-17.03.3.ce-1.el7.x86_64.rpm'
-# end
-#
-# rpm_package 'containerd' do
-#     action :install
-#     source '/tmp/containerd.io-1.2.13-3.2.el7.x86_64.rpm'
-# end
-#
-# rpm_package 'docker-ce' do
-#     action :install
-#     source '/tmp/docker-ce-19.03.10-3.el7.x86_64.rpm'
-# end
-#
-# rpm_package 'docker-ce-cli' do
-#     action :install
-#     source '/tmp/docker-ce-cli-19.03.10-3.el7.x86_64.rpm'
-# end
-#
-# service 'containerd' do
-#   action [ :enable, :start]
-# end
-#
-# service 'docker' do
-#   action [ :enable, :start]
-# end
-
-
-# Not reliable the version below so vendor it
 execute 'add_docker_repo' do
     cwd '/usr/local/src'
     user 'root'
     command 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
-end
-
-execute 'yum_update' do
-    cwd '/usr/local/src'
-    user 'root'
-    command 'yum update'
 end
 
 execute 'install_docker' do
@@ -74,6 +14,19 @@ execute 'install_docker' do
     command 'yum install -y docker-ce'
 end
 
+execute 'install_docker_cli' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y docker-ce-cli'
+end
+
+execute 'install_docker_containerd' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y containerd.io'
+end
+
+#
 execute 'install_docker_compose' do
     cwd '/usr/local/src'
     user 'root'
@@ -90,3 +43,63 @@ end
 service 'docker' do
   action [ :enable, :start]
 end
+
+
+
+
+# package 'policycoreutils-python'
+# package 'container-selinux'
+#
+# execute 'get_docker-selinux_package' do
+#     cwd '/tmp'
+#     user 'root'
+#     command 'curl -k -o /tmp/docker-ce-selinux-17.03.3.ce-1.el7.noarch.rpm https://web.ermin/br2020-packages/docker-ce-selinux-17.03.3.ce-1.el7.noarch.rpm'
+# end
+# #
+# execute 'get_containerd_package' do
+#     cwd '/tmp'
+#     user 'root'
+#     command 'curl -k -o /tmp/containerd.io-1.2.13-3.2.el7.x86_64.rpm http://web.ermin/br2020-packages/containerd.io-1.2.13-3.2.el7.x86_64.rpm'
+# end
+# #
+# execute 'get_docker_package' do
+#     cwd '/tmp'
+#     user 'root'
+#     command 'curl -k -o /tmp/docker-ce-19.03.10-3.el7.x86_64.rpm http://web.ermin/br2020-packages/docker-ce-19.03.10-3.el7.x86_64.rpm'
+# end
+# #
+# execute 'get_docker_cli_package' do
+#     cwd '/tmp'
+#     user 'root'
+#     command 'curl -k -o /tmp/docker-ce-cli-19.03.10-3.el7.x86_64.rpm http://web.ermin/br2020-packages/docker-ce-cli-19.03.10-3.el7.x86_64.rpm'
+# end
+# #
+# rpm_package 'docker-ce-selinux' do
+#     action :install
+#     source '/tmp/docker-ce-selinux-17.03.3.ce-1.el7.noarch.rpm'
+# end
+# #
+# rpm_package 'containerd' do
+#     action :install
+#     source '/tmp/containerd.io-1.2.13-3.2.el7.x86_64.rpm'
+# end
+# #
+# rpm_package 'docker-ce' do
+#     action :install
+#     source '/tmp/docker-ce-19.03.10-3.el7.x86_64.rpm'
+# end
+# #
+# rpm_package 'docker-ce-cli' do
+#     action :install
+#     source '/tmp/docker-ce-cli-19.03.10-3.el7.x86_64.rpm'
+# end
+# #
+# service 'containerd' do
+#   action [ :enable, :start]
+# end
+# #
+# service 'docker' do
+#   action [ :enable, :start]
+# end
+
+
