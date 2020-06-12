38b0984df7fe3e3d7d127f9e6e8538455b66cf15
crouchr <richard.crouch@vonage.com>
2020-06-12 23:07:19 +0100

    Docker Registry working

--- environments/production/docker-registry/setup.sh
+++ environments/production/docker-registry/setup.sh
@@ -2,15 +2,15 @@
 # https://computingforgeeks.com/install-and-configure-docker-registry-on-centos-7/
 # This script is running on the VM itself
 # Files on the Host can be accessed via the /vagrant share
-#/etc/docker-distribution/registry/config.yml.
+# https://www.centlinux.com/2019/04/configure-secure-registry-docker-distribution-centos-7.html
+
 set -e	# bomb out if any problem
 
 echo 
 echo "Started setup.sh for provisioning this node"
 
-yum -y update
-
-yum install docker-distribution
+#yum -y update
+yum -y install docker-distribution
 cp /vagrant/config.yml /etc/docker-distribution/registry/config.yml
 
 echo "Starting Docker Registry..."
