3fe699cc0a21f8b0c171cbc120b4e42fdb92637c
crouchr <richard.crouch@vonage.com>
2020-06-15 18:03:26 +0100

    Modified to docker pull

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
@@ -3,29 +3,29 @@
 execute 'install_amun_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker install -t cicd/amun:v1.0.0'
+    command 'docker pull -t cicd/amun:v1.0.0'
 end
 
 execute 'install_cowrie_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker install -t cicd/cowrie:v1.0.0'
+    command 'docker pull -t cicd/cowrie:v1.0.0'
 end
 
 execute 'install_glastofp_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker install -t cicd/glastopf:v1.0.0'
+    command 'docker pull -t cicd/glastopf:v1.0.0'
 end
 
 execute 'install_honeytrap_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker install -t cicd/honeytrap:v1.0.0'
+    command 'docker pull -t cicd/honeytrap:v1.0.0'
 end
 
 execute 'install_p0f2_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker install -t cicd/p0f2:v1.0.0'
+    command 'docker pull -t cicd/p0f2:v1.0.0'
 end
