841552de5c216911ae13ca96f762278dd940c658
crouchr <richard.crouch@vonage.com>
2020-06-15 18:02:26 +0100

    Moved all honeypot installs to docker pull

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
@@ -1,3 +1,5 @@
+# This could probably be optimised to use a loop
+
 execute 'install_amun_container' do
     cwd '/usr/local/src'
     user 'root'
@@ -8,4 +10,22 @@ execute 'install_cowrie_container' do
     cwd '/usr/local/src'
     user 'root'
     command 'docker install -t cicd/cowrie:v1.0.0'
+end
+
+execute 'install_glastofp_container' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'docker install -t cicd/glastopf:v1.0.0'
+end
+
+execute 'install_honeytrap_container' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'docker install -t cicd/honeytrap:v1.0.0'
+end
+
+execute 'install_p0f2_container' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'docker install -t cicd/p0f2:v1.0.0'
 end
