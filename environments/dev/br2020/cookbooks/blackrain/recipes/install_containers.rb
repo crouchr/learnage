12e4b1f5b416eccdac6b96ffe51cab8ade82fe5f
crouchr <richard.crouch@vonage.com>
2020-06-16 07:48:32 +0100

    Modified docker pull commands to fix bug

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
@@ -1,43 +1,44 @@
 # This could probably be optimised to use a loop
+# registry is the hostname of the registry it is a VM
 
 execute 'install_amun_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/amun:v1.0.0'
+    command 'docker pull registry/amun:1.0.0'
 end
 
 execute 'install_cowrie_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/cowrie:v1.0.0'
+    command 'docker pull registry/cowrie:1.0.0'
 end
 
 execute 'install_glastofp_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/glastopf:v1.0.0'
+    command 'docker pull registry/glastopf:1.0.0'
 end
 
 execute 'install_honeytrap_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/honeytrap:v1.0.0'
+    command 'docker pull registry/honeytrap:1.0.0'
 end
 
 execute 'install_p0f2_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/p0f2:v1.0.0'
+    command 'docker pull registry/p0f2:1.0.0'
 end
 
 execute 'install_netflow_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/netflow:v1.0.0'
+    command 'docker pull registry/netflow:1.0.0'
 end
 
 execute 'install_microlinux_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull cicd/microlinux:v1.0.0'
+    command 'docker pull registry/microlinux:1.0.0'
 end
