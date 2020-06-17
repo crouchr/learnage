a01b65f61e54bcfb35cecccabaf7c1c3cdc53832
crouchr <richard.crouch@vonage.com>
2020-06-17 06:44:09 +0100

    Fixed bug with missing port on docker pull

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_containers.rb
@@ -4,41 +4,41 @@
 execute 'install_amun_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/amun:1.0.0'
+    command 'docker pull registry:5000/amun:1.0.0'
 end
 
 execute 'install_cowrie_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/cowrie:1.0.0'
+    command 'docker pull registry:5000/cowrie:1.0.0'
 end
 
 execute 'install_glastofp_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/glastopf:1.0.0'
+    command 'docker pull registry:5000/glastopf:1.0.0'
 end
 
 execute 'install_honeytrap_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/honeytrap:1.0.0'
+    command 'docker pull registry:5000/honeytrap:1.0.0'
 end
 
 execute 'install_p0f2_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/p0f2:1.0.0'
+    command 'docker pull registry:5000/p0f2:1.0.0'
 end
 
 execute 'install_netflow_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/netflow:1.0.0'
+    command 'docker pull registry:5000/netflow:1.0.0'
 end
 
 execute 'install_microlinux_container' do
     cwd '/usr/local/src'
     user 'root'
-    command 'docker pull registry/microlinux:1.0.0'
+    command 'docker pull registry:5000/microlinux:1.0.0'
 end
