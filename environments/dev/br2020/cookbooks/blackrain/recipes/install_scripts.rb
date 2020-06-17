d7bdb6f17af898b0f8d57a9761016d4ef351eda2
crouchr <richard.crouch@vonage.com>
2020-06-17 07:00:27 +0100

    Relocated scripts

--- environments/dev/br2020/cookbooks/blackrain/recipes/install_scripts.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/install_scripts.rb
@@ -1,19 +1,29 @@
+
+# scripts used during debugging/development etc
 cookbook_file "/opt/br2020/bin/start_honeypots.sh" do
-  source "start_honeypots.sh"
+  source "scripts/start_honeypots.sh"
   mode "0755"
 end
 
-cookbook_file "/opt/br2020/bin/rules.sh" do
-  source "rules.sh"
-  mode "0644"
+cookbook_file "/opt/br2020/bin/run_portainer.sh" do
+  source "scripts/run_portainer.sh"
+  mode "0755"
 end
 
+# scripts for starting up services
 cookbook_file "/opt/br2020/etc/br2020.service" do
-  source "br2020.service"
+  source "scripts/br2020.service"
   mode "0644"
 end
 
+# docker compose scripts
 cookbook_file "/opt/br2020/etc/br2020.yml" do
-  source "br2020.yml"
+  source "scripts/br2020.yml"
+  mode "0644"
+end
+
+# misc scripts
+cookbook_file "/opt/br2020/bin/rules.sh" do
+  source "scripts/rules.sh"
   mode "0644"
 end
