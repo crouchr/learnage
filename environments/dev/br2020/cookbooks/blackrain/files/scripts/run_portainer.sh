f4526239a25f844a7009c5bba38fca8792df60d6
crouchr <richard.crouch@vonage.com>
2020-06-17 08:10:46 +0100

    Fixed bug in portainer

--- environments/dev/br2020/cookbooks/blackrain/files/scripts/run_portainer.sh
+++ environments/dev/br2020/cookbooks/blackrain/files/scripts/run_portainer.sh
@@ -1,7 +1,7 @@
 # run using vagrant user - no need for sudo
 docker run -d \
 -p 9000:9000 \
--name portainer \
+--name portainer \
 -v /var/run/docker.sock:/var/run/docker.sock \
 portainer/portainer \
 -H unix:///var/run/docker.sock
