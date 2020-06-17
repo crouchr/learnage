20fff5b7aed1ca151a542e1a1a10ebf9c862a2d8
crouchr <richard.crouch@vonage.com>
2020-06-17 07:04:30 +0100

    Fixed bug in run_portainer.sh

--- environments/dev/br2020/cookbooks/blackrain/files/scripts/run_portainer.sh
+++ environments/dev/br2020/cookbooks/blackrain/files/scripts/run_portainer.sh
@@ -1,7 +1,6 @@
-# This must be run as root
-# This does not work - UI fires up but issue with tthe docker.sock
+# This can be run as vagrant
 docker run -d \
 -p 9000:9000 \
 -v /var/run/docker.sock:/var/run/docker.sock \
 portainer/portainer \
--H unix://var/run/docker.sock
+-H unix:///var/run/docker.sock
