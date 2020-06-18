6ea8e92f74a26ed4472126c98e19f350556903ca
crouchr <richard.crouch@vonage.com>
2020-06-18 08:12:33 +0100

    box build is to install telegraf only - chef will configure it

--- common-packer-scripts/install-telegraf.sh
+++ common-packer-scripts/install-telegraf.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eux
-
+# Install telgraf on node - Chef will be used to configure it
 # Put influxdb on another node
 
 # https://www.digitalocean.com/community/tutorials/how-to-monitor-system-metrics-with-the-tick-stack-on-centos-7
@@ -9,9 +9,3 @@ sudo wget --no-check-certificate \
    -O /etc/yum.repos.d/influxdata.repo
 
 sudo yum -y install telegraf
-
-sudo wget --no-check-certificate \
-   http://web.ermin/br2020-packages/telegraf.conf \
-   -O /etc/telegraf/telegraf.conf
-
-# start the service
