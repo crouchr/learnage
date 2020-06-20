9fa1ab8984d102c926af1a4b438c94ad96594a76
crouchr <richard.crouch@vonage.com>
2020-06-20 11:32:12 +0100

    Added initial version of cowrie honeypot

--- environments/dev/br2020/cookbooks/blackrain/files/scripts/update_all_containers.sh
+++ environments/dev/br2020/cookbooks/blackrain/files/scripts/update_all_containers.sh
@@ -2,11 +2,20 @@
 # Extend this to actually pull 'latest' when pipeline support semantic versioning
 echo "Pulling latest containers..."
 sudo docker pull registry:5000/amun:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/cowrie:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/glastopf:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/honeytrap:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/p0f2:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/netflow:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/microlinux:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/scantools:1.0.0
+echo "-----------------------------------"
 sudo docker pull registry:5000/dionaea:1.0.0
+
