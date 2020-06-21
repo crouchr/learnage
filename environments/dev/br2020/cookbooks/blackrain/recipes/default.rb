ae635e363d9fe0752f43a07ae17f0ae23214809c
crouchr <richard.crouch@vonage.com>
2020-06-21 08:39:58 +0100

    Temp disable honeypot until dionaea is working

--- environments/dev/br2020/cookbooks/blackrain/recipes/default.rb
+++ environments/dev/br2020/cookbooks/blackrain/recipes/default.rb
@@ -105,11 +105,12 @@ directory '/data/honeytrap' do
   action :create
 end
 
+# Only run this once honeytrap is IGNORING certain ports
 # See Virtual Honeypots book p200 : Queue all new incoming requests for use by Honeytrap
-execute 'mod_iptables_honeytrap' do
-    user 'root'
-    command 'iptables -A INPUT -i enp0s8 -p tcp --syn -m state --state NEW -j QUEUE'
-end
+#execute 'mod_iptables_honeytrap' do
+#    user 'root'
+#    command 'iptables -A INPUT -i enp0s8 -p tcp --syn -m state --state NEW -j QUEUE'
+#end
 
 # AMUN
 directory '/data/amun' do
