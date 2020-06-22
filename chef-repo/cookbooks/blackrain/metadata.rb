971a536c4e2fd0ea7916f3ae89b564839348f28a
crouchr <richard.crouch@vonage.com>
2020-06-22 21:49:10 +0100

    Added initial version of cowrie honeypot

--- chef-repo/cookbooks/blackrain/metadata.rb
+++ chef-repo/cookbooks/blackrain/metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'richard.crouch100@gmail.com'
 license          'All rights reserved'
 description      'Installs/Configures BlackRain2020'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.2.0'
---
+++ chef-repo/cookbooks/blackrain/recipes/install_docker_ce.rb
@@ -0,0 +1,50 @@
+# Not always reliable ?
+# Unreliable at moment so commented out temporarily
+# https://download.docker.com/linux/centos/7/x86_64/stable/Packages/ - contains rpms - todo - use the rpms
+# Not needed as Docker now built into the rch-centos7-docker box image
+
+execute 'add_docker_repo' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo'
+end
+
+execute 'install_docker' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y docker-ce'
+end
+
+execute 'install_docker_cli' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y docker-ce-cli'
+end
+
+execute 'install_docker_containerd' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y containerd.io'
+end
+
+#
+execute 'install_docker_compose' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'yum install -y docker-compose'
+end
+
+# Allow vagrant user to run docker
+execute 'add_vagrant_docker' do
+    cwd '/usr/local/src'
+    user 'root'
+    command 'usermod -aG docker vagrant'
+end
+
+service 'docker' do
+  action [ :enable, :start]
+end
+
+
+
+
