dcf5b1bacbcf976ad2ce6a988ad0a15fe39eb7fc
crouchr <richard.crouch@vonage.com>
2020-06-22 19:39:32 +0100

    Added metadata for blackrain recipes

---
+++ environments/dev/br2020/cookbooks/blackrain/metadata.rb
@@ -0,0 +1,7 @@
+name             'blackrain'
+maintainer       'Richard Crouch'
+maintainer_email 'richard.crouch100@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures BlackRain2020'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
