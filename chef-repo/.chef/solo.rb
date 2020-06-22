b498334f54ca139d5326b91d1a2aa47e2e73203f
crouchr <richard.crouch@vonage.com>
2020-06-22 21:45:38 +0100

    dev/br2020 now referencing recipes in chef-repo

--- chef-repo/.chef/solo.rb
+++ chef-repo/.chef/solo.rb
@@ -1,2 +1,3 @@
-# Relative to inside the VM
-cookbook_path "/vagrant/learnage/environments/dev/br2020/cookbooks"
+# Relative to inside the VM - via the shared folders
+#cookbook_path "/vagrant/learnage/environments/dev/br2020/cookbooks"
+cookbook_path "/vagrant/learnage/chef-repo/cookbooks"
