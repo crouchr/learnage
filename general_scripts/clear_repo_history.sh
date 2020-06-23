e69fafafbe5337476c4657eb5abe139cc97817fd
crouchr <richard.crouch@vonage.com>
2020-06-23 06:06:40 +0100

    Comment

--- general_scripts/clear_repo_history.sh
+++ general_scripts/clear_repo_history.sh
@@ -1,4 +1,6 @@
 # ref : https://www.willandskill.se/en/deleting-your-git-commit-history-without-removing-repo-on-github-bitbucket/
+
+# This has not been tested
 cd /home/crouchr/PycharmProjects/learnage
 rm -rf .gitgit ulimit git add .
 git commit -m "Removed history"
