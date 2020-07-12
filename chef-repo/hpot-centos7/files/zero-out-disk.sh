239d180be0dc1fa0d9bd13109d8d259d3fa03dfd
crouchr <richard.crouch@vonage.com>
2020-07-12 16:11:49 +0100

    Added honeywall recipe

---
+++ chef-repo/hpot-centos7/files/zero-out-disk.sh
@@ -0,0 +1,5 @@
+echo "Zeroing out empty space."
+# This forces the filesystem to reclaim space from deleted files
+dd bs=1M if=/dev/zero of=/var/tmp/zeros || :
+rm -f /var/tmp/zeros
+echo "(Don't worry -- that out-of-space error was expected.)"
