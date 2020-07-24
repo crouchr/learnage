8c3bcc43feee0a6b082ff96094b6553728b77d8c
crouchr <richard.crouch@vonage.com>
2020-07-24 13:42:38 +0100

    Updated chef to include mod_sec, php

--- chef-repo/cookbooks/hpot-centos7/recipes/install_httpd.rb
+++ chef-repo/cookbooks/hpot-centos7/recipes/install_httpd.rb
@@ -1,4 +1,4 @@
-
+# install Apache and PHP
 #cp /vagrant/apache/minimal-index.html /var/www/html/index.html
 #chown apache:apache /var/www/html/index.html
 #chmod 755 /var/www/html/index.html#
@@ -8,19 +8,47 @@ package 'httpd'
 package 'httpd-devel'
 package 'mod_ssl'
 package 'php'
+package 'php-mysql'
 
-cookbook_file "/etc/httpd/httpd.conf" do
-   source "hpot-httpd.conf"
-   mode "0644"
+# need to add mod_sec
+
+cookbook_file '/etc/httpd/httpd.conf' do
+   source 'hpot-httpd.conf'
+   mode  '0644'
    owner 'apache'
    group 'apache'
 end
 
-cookbook_file "/var/www/html/index.html" do
-   source "hpot-index.html"
-   mode "0755"
+cookbook_file '/var/www/html/index.html' do
+   source 'hpot-index.html'
+   mode  '0755'
    owner 'apache'
    group 'apache'
 end
 
-# Copy a load of stuff from ppot project - i.e. download a zip file and unbundle
+# Add basic tet file for PHP (& haxxors)
+cookbook_file '/var/www/html/info.php' do
+   source 'info.php'
+   mode  '0755'
+   owner 'apache'
+   group 'apache'
+end
+
+# The dummy web-site used to make the honeypot look semi-legit
+remote_file '/tmp/hpot-tmp/ppots.tar.gz' do
+  source        'http://web.ermin.lan/br2020-packages/ppots.tar.gz'
+  owner         'root'
+  group         'root'
+  mode          '0755'
+  action        :create
+end
+
+archive_file 'unarchive_ppots_website' do
+  destination      '/var/www'
+  group            'apache'
+  mode             '755'
+  overwrite        true
+  owner            'apache'
+  path             '/tmp/hpot-tmp/ppots.tar.gz'
+  action           :extract
+end
---
+++ chef-repo/cookbooks/hpot-centos7/recipes/install_waf.rb
@@ -0,0 +1,15 @@
+package 'mod_security'
+package 'mod_security_crs'
+package 'mod_security-mlogc'
+
+cookbook_file '/etc/httpd/conf.d/mod_security.conf' do
+   source 'mod_security.conf'
+   mode  '0644'
+   owner 'apache'
+   group 'apache'
+end
+
+# restart HTTPd
+service 'httpd' do
+  action :restart
+end
