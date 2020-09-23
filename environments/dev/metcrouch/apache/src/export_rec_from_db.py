eaf7077c8a7737c9e9d1f5fef40fa949e68512e2
crouchr <richard.crouch@vonage.com>
2020-09-23 08:15:31 +0100

    snapshot

--- environments/dev/metcrouch/apache/src/export_rec_from_db.py
+++ environments/dev/metcrouch/apache/src/export_rec_from_db.py
@@ -16,13 +16,15 @@ def main():
     mydb, mycursor = connect_db.connect_database(database_name)
 
     low_id  = input("Lowest ID  (to export) : ")
+    if low_id == "":
+        low_id = 1
     high_id = input("Highest ID (to export) : ")
     location = input("Location : ")
     if location == '':
         location = 'Stockcross, UK'
 
     sql = """SELECT pressure, wind_deg, temp FROM %s WHERE location = %s AND id >= %s AND id <= %s"""
-    sql = "SELECT id, pressure, wind_deg, temp, rain, humidity FROM " + table_name.__str__() + " WHERE location = '" + location + "' AND id >= " + low_id.__str__() + " AND id <= " + high_id.__str__()
+    sql = "SELECT id, pressure, wind_deg, temp, rain, humidity, wind_speed FROM " + table_name.__str__() + " WHERE location = '" + location + "' AND id >= " + low_id.__str__() + " AND id <= " + high_id.__str__()
     print("SQL : " + sql)
     mycursor.execute(sql)
     records = mycursor.fetchall()
@@ -44,18 +46,20 @@ def main():
 
     #
     fp_out = open(export_file, "w")
-    header = "id\tpressure\twind_deg\trainx100\twind_deg_diff\ttemp\thumidity\n"
+    header = "id\tpressure\twind_deg\trainx100\twind_speedx100\twind_deg_diff\ttemp\thumidity\n"
     fp_out.write(header)
+
     for row in records:
         id = row[0]
         pressure = (row[1] - 950) * 4   # multiplication to make visible in graph only
         wind_deg = row[2]
         temp = row[3]
-        rain = row[4] * 100 # make it appear in the graph
+        rain = row[4] * 100             # make it appear in the graph
         humidity = row[5]
+        wind_speed = row[6] * 8        # multiplication to make visible in graph only
 
         wind_deg_diff = wind_deg - last_wind_deg
-        csv_rec = id.__str__() + "\t" + pressure.__str__() + "\t" + wind_deg.__str__() + "\t" + rain.__str__()  + "\t" + wind_deg_diff.__str__() + "\t" + temp.__str__() + "\t" + humidity.__str__() + "\n"
+        csv_rec = id.__str__() + "\t" + pressure.__str__() + "\t" + wind_deg.__str__() + "\t" + rain.__str__() + "\t" + wind_speed.__str__() + "\t" + wind_deg_diff.__str__() + "\t" + temp.__str__() + "\t" + humidity.__str__() + "\n"
         fp_out.write(csv_rec)
         last_wind_deg = wind_deg
         print(csv_rec)
