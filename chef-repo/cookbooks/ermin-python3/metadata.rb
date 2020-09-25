57fca33aeb23d00408782846c38012178d54a3bf
crouchr <richard.crouch@vonage.com>
2020-09-25 13:35:12 +0100

    Snapshot

--- chef-repo/cookbooks/ermin-python3/metadata.rb
+++ chef-repo/cookbooks/ermin-python3/metadata.rb
@@ -7,7 +7,7 @@ long_description 'Installs/Configures ermin-python3'
 version '0.1.0'
 chef_version '>= 14.0'
 
-include_recipe 'ermin-centos7'
+depends 'ermin-centos7'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
