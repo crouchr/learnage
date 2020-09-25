fbab6e32768b55ec418e7d7429c5c087ed9945ac
crouchr <richard.crouch@vonage.com>
2020-09-25 13:34:45 +0100

    Snapshot

---
+++ chef-repo/cookbooks/ermin-centos7/README.md
@@ -0,0 +1,4 @@
+# ermin-centos7
+
+TODO: Enter the cookbook description here.
+
---
+++ chef-repo/cookbooks/ermin-centos7/recipes/default.rb
@@ -0,0 +1,7 @@
+#
+# Cookbook:: ermin-centos7
+# Recipe:: default
+#
+# Copyright:: 2020, The Authors, All Rights Reserved.
+
+package 'epel-release'
