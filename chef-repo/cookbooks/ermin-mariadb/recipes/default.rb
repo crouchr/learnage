7c9528c9b71e6f2431aa02f46929f4eca75e419b
crouchr <richard.crouch@vonage.com>
2020-09-25 12:10:29 +0100

    Snapshot

--- chef-repo/cookbooks/ermin-mariadb/recipes/default.rb
+++ chef-repo/cookbooks/ermin-mariadb/recipes/default.rb
@@ -9,5 +9,24 @@
 # yum install -y mariadb-server
 # pip3 install mysql-connector-python
 # pip3 install mariadb
+# Needed for mariadb-connector-c
+
+execute 'install_okay' do
+    user 'root'
+    command 'yum install -y http://repo.okay.com.mx/centos/7/x86_64/release/okay-release-1-1.noarch.rpm'
+end
 
 package 'mariadb-server'
+package 'mariadb-libs'
+package 'mariadb-devel'
+package 'mariadb-connector-c'
+
+execute 'install_mysql_connector' do
+    user 'root'
+    command 'pip3 install mysql-connector-python'
+end
+
+execute 'install_mariadb' do
+    user 'root'
+    command 'pip3 install mariadb'
+end
--- chef-repo/cookbooks/ermin-python3/metadata.rb
+++ chef-repo/cookbooks/ermin-python3/metadata.rb
@@ -7,6 +7,8 @@ long_description 'Installs/Configures ermin-python3'
 version '0.1.0'
 chef_version '>= 14.0'
 
+include_recipe 'ermin-centos7'
+
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
