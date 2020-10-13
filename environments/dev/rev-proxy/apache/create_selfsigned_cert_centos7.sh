dc70993ef181708cba9c0d56e2ba9da086e7c190
crouchr <richard.crouch@vonage.com>
2020-10-13 19:25:49 +0100

    Added self-signed cert

---
+++ environments/dev/rev-proxy/apache/create_selfsigned_cert_centos7.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# https://www.digitalocean.com/community/tutorials/how-to-create-an-ssl-certificate-on-apache-for-centos-7
+# execute this manually
+set -e	# bomb out if any problem
+
+#
+cd /etc/ssl
+mkdir /etc/ssl/private
+chmod 700 /etc/ssl/private
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout /etc/ssl/private/apache-selfsigned.key -out /etc/ssl/certs/apache-selfsigned.crt -subj '/C=GB/ST=London/L=London/O=NetOps/CN=rev-proxy.ermin'
+openssl dhparam -out /etc/ssl/certs/dhparam.pem 2048
+cat /etc/ssl/certs/dhparam.pem | sudo tee -a /etc/ssl/certs/apache-selfsigned.crt
--- environments/dev/rev-proxy/setup.sh
+++ environments/dev/rev-proxy/setup.sh
@@ -11,20 +11,37 @@ echo "[+] Started setup.sh for provisioning this node"
 #echo "[+] Adding glastopf to hosts file..."
 #echo "192.168.1.62 glastopf.ermin glastopf" >> /etc/hosts
 
+echo "[+] Adding XW6600 webserver to hosts file..."
+echo "192.168.1.15 erminserver.ermin erminserver" >> /etc/hosts
+
 # Check for patch updates - slows up boot so need a way of avoiding this
 yum update -y --disableplugin=fastestmirror
 systemctl restart sshd
 
 yum install -y httpd httpd-devel mod_ssl
 
+echo "[+] Create directory for self-signed certificate..."
+cd /etc/ssl
+mkdir /etc/ssl/private
+chmod 700 /etc/ssl/private
+
 echo "[+] Copying core (root-owned) web server configuration and content..."
-cp /vagrant/apache/minimal-index.html /var/www/html/index.html
+cp /vagrant/apache/index.html /var/www/html/index.html
 chown apache:apache /var/www/html/index.html
 chmod 755 /var/www/html/index.html
 
 echo "[+] Copying Apache configuration..."
 cp /vagrant/apache/httpd.conf /etc/httpd/conf/
 cp /vagrant/apache/proxy.conf /etc/httpd/conf.d/
+cp /vagrant/apache/ssl.conf /etc/httpd/conf.d/
+
+echo "[+] Copy SSL cert generation script (run manually)..."
+cp /vagrant/apache/create_selfsigned_cert_centos7.sh /tmp/
+
+#echo "[+] Copy self-signed certificate..."
+#ls /vagrant/certs
+yes | cp -rf /vagrant/certs/*.crt /etc/ssl/certs
+yes | cp -rf /vagrant/certs/*.key /etc/ssl/private
 
 echo "[+] Adding PaperTrail destination for rsyslog..."
 echo "# The following entry was created during box provisioning" > /etc/rsyslog.d/95-papertrail.conf
