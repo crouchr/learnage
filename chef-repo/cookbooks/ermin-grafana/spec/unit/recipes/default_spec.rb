78e7186d933a9e2e3563736f7ff177f00b132f1f
crouchr <richard.crouch@vonage.com>
2020-10-22 17:30:26 +0100

    Cookstyle fixes

--- chef-repo/cookbooks/ermin-grafana/spec/unit/recipes/default_spec.rb
+++ chef-repo/cookbooks/ermin-grafana/spec/unit/recipes/default_spec.rb
@@ -12,14 +12,10 @@
 require 'spec_helper'
 
 describe 'ermin-grafana::default' do
-  let (:chef_instance) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7') }
-  let (:chef_run) { chef_instance.converge(described_recipe) }
+  let(:chef_instance) { ChefSpec::SoloRunner.new(platform: 'centos', version: '7') }
+  let(:chef_run) { chef_instance.converge(described_recipe) }
 
   it 'installs grafana' do
     expect(chef_run).to install_package('grafana')
   end
 end
-
-
-
-
