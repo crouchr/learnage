cf05c26bc4e7ccc0968c170c9832dac20432801b
crouchr <richard.crouch@vonage.com>
2020-11-10 11:06:32 +0000

    Added scripts

---
+++ environments/production/docker-registry/scripts/view_images_in_registry.sh
@@ -0,0 +1,7 @@
+# list the catalog
+curl http://192.168.1.109:5000/v2/_catalog
+
+# list the versions
+curl http://192.168.1.109:5000/v2/vyos-graphviz/tags/list
+curl http://192.168.1.109:5000/v2/nvm-chef-app/tags/list
+curl http://192.168.1.109:5000/v2/jenkins-nvm/tags/list
