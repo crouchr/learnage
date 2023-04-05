85ff5d27c3dfd59e9c4bd113f804cf45406b37b3
Richard Crouch <richard.crouch@protonmail.com>
2023-04-05 11:20:18 +0100

    vcc-basic : Updated README.md

--- chef-repo/cookbooks/vcc-basic/README.md
+++ chef-repo/cookbooks/vcc-basic/README.md
@@ -1,4 +1,12 @@
 # vcc-basic
 
-TODO: Enter the cookbook description here.
+This cookbook is a useful example of running a dev 
+and prod environment with correspondingly different inspec tests.
+
+It also uses berksfile.
+
+Basically a simple project that is 'close' to being a VCC cookbook project and can be used for minimal
+environment for testing Chef concepts etc.
+
+See 'Learning Chef' page 271 for corect command to create a cookbook folder structure, including Berkfile.
 
--- chef-repo/cookbooks/vcc-basic/test/integration/prod/check_httpd_admin_ip_access_spec.rb
+++ chef-repo/cookbooks/vcc-basic/test/integration/prod/check_httpd_admin_ip_access_spec.rb
@@ -2,11 +2,11 @@
 # Check the permitted IPs for /Auth/Admin
 describe command('python /usr/local/bin/networks_regex_tool.py /etc/httpd/apache.conf "(\"\/Auth\/Admin\">\s+Require ip\s+)((\d+\.\d+\.\d+\.\d+(\/\d+)?\s)+)" 1') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq '1.1.1.1 2.2.2.2 5.5.5.5 6.6.6.6' }
+  its('stdout') { should eq '1.1.1.1 2.2.2.2' }
 end
 
 # Check the permitted IPs for /CallCentre/admin
 describe command('python /usr/local/bin/networks_regex_tool.py /etc/httpd/apache.conf "(\"\/CallCentre\/Admin\">\s+Require ip\s+)((\d+\.\d+\.\d+\.\d+(\/\d+)?\s)+)" 1') do
   its('exit_status') { should eq 0 }
-  its('stdout') { should eq '1.1.1.1 2.2.2.2 5.5.5.5 6.6.6.6' }
+  its('stdout') { should eq '1.1.1.1 2.2.2.2' }
 end
