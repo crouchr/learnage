9b1341e271ab98d49b7cf1ffa76bd0f174badb6d
Richard Crouch <richard.crouch@protonmail.com>
2023-10-06 14:07:48 +0100

    Updates

--- environments/production/chef-server/setup.sh
+++ environments/production/chef-server/setup.sh
@@ -1,27 +1,32 @@
 #!/usr/bin/env bash
 # https://docs.chef.io/server/install_server/
+# Chef uses Nginx, Postgres Dbase, RabbitMQ
 
 set -e	# bomb out if any problem
 
 echo 
-echo "Started setup.sh for provisioning this node"
+echo "[+] Started setup.sh for provisioning this node"
 whoami
 
-# Re-enable updates once working
+# Re-enable updates once working...
 #yum -y update
 mkdir -p /home/vagrant/certs
 
-echo 'Install Chef Server...'
+echo '[+] Installing Chef Server...'
 cp /vagrant/chef-server-core-12.19.31-1.el7.x86_64.rpm /tmp/
 yum -y localinstall /tmp/chef-server-core-12.19.31-1.el7.x86_64.rpm
 
-echo 'Reconfigure Chef Server - runs up the various daemons...'
+echo '[+] Configuring Chef Server - runs up the various daemons...'
 chef-server-ctl reconfigure
 
-echo 'Create user account...'
+# An RSA private key is generated automatically.
+# This is the chef-validator key and should be saved to a safe location.
+# The --filename option will save the RSA private key to the specified absolute path.
+echo '[+] Creating user account...'
 chef-server-ctl user-create crouchr Richard Crouch richard.crouch@protonmail.com 'abc123' --filename /home/vagrant/certs/crouchr.pem
 
-#echo 'Create organisation...'
+echo '[+] Creating organisation...'
+# --filename is where the certs will be written to
 chef-server-ctl org-create br2023 'BlackRain2023' --association_user crouchr --filename /home/vagrant/certs/br2023.pem
 chef-server-ctl install chef-manage
 chef-server-ctl reconfigure
@@ -35,5 +40,5 @@ chef-manage-ctl reconfigure
 #systemctl enable telegraf
 #systemctl start telegraf
 
-echo "Finished setup.sh OK for provisioning this node"
+echo "[+] Finished setup.sh OK for provisioning this node"
 echo
