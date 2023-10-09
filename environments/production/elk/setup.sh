7fca3ef19ca160f816a7b170e66afbe318711900
Richard Crouch <richard.crouch@protonmail.com>
2023-10-09 20:50:37 +0100

    ELK update

--- environments/production/elk/setup.sh
+++ environments/production/elk/setup.sh
@@ -11,25 +11,33 @@ yum -y install java-1.8.0-openjdk.x86_64
 # cp /vagrant/config.yml /etc/docker-distribution/registry/config.yml
 
 # ELK
-wget wget -O /tmp/elasticsearch-7.9.2-x86_64.rpm https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
-rpm -ivh /tmp/elasticsearch-7.9.2-x86_64.rpm
+#wget -O /tmp/elasticsearch-7.9.2-x86_64.rpm https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
+#rpm -ivh /tmp/elasticsearch-7.9.2-x86_64.rpm
 #yum -y localinstall elasticsearch-7.9.2-x86_64.rpm
+
+echo 'Copy packages...'
+cp /vagrant/packages/*.rpm /tmp/
+rpm -ivh /tmp/logstash-oss-7.9.2.rpm
+rpm -ivh /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
+rpm -ivh /tmp/kibana-oss-7.9.2-x86_64.rpm
+
+echo 'Copy configuration files...'
+cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/
 
 # Telegraf
 #wget https://dl.influxdata.com/telegraf/releases/telegraf-1.8.3-1.x86_64.rpm
 #yum -y localinstall telegraf-1.8.3-1.x86_64.rpm
 #cp /vagrant/telegraf.conf /etc/telegraf/telegraf.conf
 
-#echo "Starting Elasticsearch..."
-#sudo systemctl enable elasticsearch.service
+echo "Enable ELK services..."
+sudo systemctl enable logstash.service
+sudo systemctl enable elasticsearch.service
+sudo systemctl enable kibana.service
 
-#echo "Starting Telegraf Agent..."
-#systemctl enable telegraf
-#systemctl start telegraf
-
-#echo "Starting Docker Registry..."
-#systemctl start docker-distribution
-#systemctl enable docker-distribution
+echo "Start ELK services..."
+sudo systemctl start logstash.service
+sudo systemctl start elasticsearch.service
+sudo systemctl start kibana.service
 
 echo "Finished setup.sh OK for provisioning this node"
 echo
