9353491fa6ce4b54c987149b574036b92f45f77e
Richard Crouch <richard.crouch@protonmail.com>
2023-10-10 14:10:13 +0100

    Added haproxy

--- environments/production/haproxy/setup.sh
+++ environments/production/haproxy/setup.sh
@@ -9,34 +9,19 @@ echo "Started setup.sh for provisioning this node"
 yum update -y --disableplugin=fastestmirror
 yum -y install joe haproxy
 
-#echo 'Copy packages...'
-#cp /vagrant/packages/*.rpm /tmp/
-#rpm -ivh /tmp/logstash-oss-7.9.2.rpm
-#rpm -ivh /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
-#rpm -ivh /tmp/kibana-oss-7.9.2-x86_64.rpm
-
-#echo 'Copy configuration files...'
-#cp /vagrant/config/ip_to_honeypot_name_mapping.csv /tmp/
-#cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/
-#cp /vagrant/config/logstash.conf /etc/logstash/conf.d/
-#cp /vagrant/config/kibana.yml /etc/kibana/
+echo 'Copy configuration files...'
+cp /vagrant/config/haproxy.cfg /etc/haproxy/
 
 # Telegraf
 #wget https://dl.influxdata.com/telegraf/releases/telegraf-1.8.3-1.x86_64.rpm
 #yum -y localinstall telegraf-1.8.3-1.x86_64.rpm
 #cp /vagrant/telegraf.conf /etc/telegraf/telegraf.conf
 
-chkconfig haproxy on
+echo "Enable HAProxy services..."
+sudo systemctl enable haproxy
 
-#echo "Enable ELK services..."
-#sudo systemctl enable logstash.service
-#sudo systemctl enable elasticsearch.service
-#sudo systemctl enable kibana.service
-
-#echo "Start ELK services..."
-#sudo systemctl start elasticsearch.service
-#sudo systemctl start logstash.service
-#sudo systemctl start kibana.service
+echo "Start HAProxy services..."
+sudo systemctl start haproxy
 
 echo "Finished setup.sh OK for provisioning this node"
 echo
