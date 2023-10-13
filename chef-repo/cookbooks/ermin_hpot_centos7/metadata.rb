389e93dd71823fa167b641a51840b3b91451a08e
Richard Crouch <richard.crouch@protonmail.com>
2023-10-13 19:19:00 +0100

    Refactored cookbook names to reflect VCC format

--- chef-repo/cookbooks/ermin_hpot_centos7/metadata.rb
+++ chef-repo/cookbooks/ermin_hpot_centos7/metadata.rb
@@ -1,19 +1,20 @@
-name 'vcc-basic'
+name 'ermin-hpot-centos7'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures vcc-basic'
+description 'Installs/Configures ermin-hpot-centos7'
+long_description 'Installs/Configures ermin-hpot-centos7'
 version '0.1.0'
-chef_version '>= 16.0'
+chef_version '>= 14.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/vcc-basic/issues'
+# issues_url 'https://github.com/<insert_org_here>/hpot-centos7/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/vcc-basic'
+# source_url 'https://github.com/<insert_org_here>/hpot-centos7'
--- chef-repo/cookbooks/ermin_vcc_basic/metadata.rb
+++ chef-repo/cookbooks/ermin_vcc_basic/metadata.rb
@@ -1,20 +1,19 @@
-name 'hpot-centos7'
+name 'ermin_vcc_basic'
 maintainer 'The Authors'
 maintainer_email 'you@example.com'
 license 'All Rights Reserved'
-description 'Installs/Configures hpot-centos7'
-long_description 'Installs/Configures hpot-centos7'
+description 'Installs/Configures ermin_vcc_basic'
 version '0.1.0'
-chef_version '>= 14.0'
+chef_version '>= 16.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
 #
-# issues_url 'https://github.com/<insert_org_here>/hpot-centos7/issues'
+# issues_url 'https://github.com/<insert_org_here>/vcc-basic/issues'
 
 # The `source_url` points to the development repository for this cookbook.  A
 # `View Source` link will be displayed on this cookbook's page when uploaded to
 # a Supermarket.
 #
-# source_url 'https://github.com/<insert_org_here>/hpot-centos7'
+# source_url 'https://github.com/<insert_org_here>/vcc-basic'
