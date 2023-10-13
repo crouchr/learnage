d8e034855a96e5aebf547bcc1ad1e404781de839
Richard Crouch <richard.crouch@protonmail.com>
2023-10-14 00:01:04 +0100

    Added cloud-init configuration to test VBOX

--- chef-repo/cookbooks/node/bootstrap.sh
+++ chef-repo/cookbooks/node/bootstrap.sh
@@ -5,9 +5,9 @@ hostnamectl set-hostname testbox.ermin.com
 
 cat >> /etc/hosts <<EOL
 # Added during Chef Vagrant run
-192.168.1.70  chef chef.ermin.com
-192.168.1.5   kube kube.ermin.com
-192.168.1.6   j1900 j1900.ermin.com
+192.168.1.70 chef chef.ermin.com
+192.168.1.5  kube kube.ermin.com
+192.168.1.6  j1900 j1900.ermin.com
 EOL
 
 echo '<<< exited setup.sh...'
--- environments/production/cloud-init-test/setup.sh
+++ environments/production/cloud-init-test/setup.sh
@@ -5,12 +5,21 @@
 set -e	# bomb out if any problem
 
 echo 
-echo "Started setup.sh for provisioning squid node"
+echo "Started setup.sh for provisioning node"
 
 # Check for patch updates - slows up boot so need a way of avoiding this
 yum update -y --disableplugin=fastestmirror
 
 yum install -y joe cloud-init
+
+# These should be added in the VBOX Packer build in the future
+cat >> /etc/hosts <<EOL
+# Added during Vagrant run
+192.168.1.6  j1900 j1900.ermin.lan
+192.168.1.70 chef chef.ermin.com
+EOL
+
+copy config/10_datasource.cfg /etc/cloud/cloud.cfg.d/10_datasource.cfg
 
 echo "Finished setup.sh OK for provisioning this squid node"
 echo
