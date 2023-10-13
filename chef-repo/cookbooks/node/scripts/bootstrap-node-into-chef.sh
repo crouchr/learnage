932ae1c06133fc29564e3de62df7ed7bbf2dcaab
Richard Crouch <richard.crouch@protonmail.com>
2023-10-13 17:43:32 +0100

    Added a script to bootstrap a node into Chef Server

--- chef-repo/cookbooks/node/scripts/bootstrap-node-into-chef.sh
+++ chef-repo/cookbooks/node/scripts/bootstrap-node-into-chef.sh
@@ -1,5 +1,8 @@
 # Run this script to bootstrap the Test Kitchen node into my Chef Server
 # use with chef-client 12+
 knife bootstrap --sudo --connection-user vagrant --connection-password vagrant \
--N default-centos7box.vagrantup.com \
---ssh-verify-host-key default-centos7box.vagrantup.com
+--node-name default-centos7box.vagrantup.com \
+--no-host-key-verify \
+default-centos7box.vagrantup.com
+
+
