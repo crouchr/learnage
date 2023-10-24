f3a4c6922be16b2c5f4bdfbf84fe31b61701a555
Richard Crouch <richard.crouch@protonmail.com>
2023-10-24 22:23:51 +0100

    First version of ermin-ppots cookbook

--- chef-repo/cookbooks/ermin-ppots/recipes/default.rb
+++ chef-repo/cookbooks/ermin-ppots/recipes/default.rb
@@ -6,7 +6,7 @@
 # ppots.com : install basic ppots web-site
 
 remote_directory "/var/www/ppots.com/public_html" do
-  source 'html' # <-- this is your directory in files/default/local_directory
+  source 'html' # <-- this is your directory in files/local_directory
   files_owner 'apache'
   files_group 'apache'
   files_mode '0750'
