66e2cd38d5d25379100de3598f8329068a3b5b0b
crouchr <richard.crouch@protonmail.com>
2024-03-08 07:47:47 +0000

    Added mariadb and grafana

--- vagrant_environments/production/tradr-iceberg/setup.sh
+++ vagrant_environments/production/tradr-iceberg/setup.sh
@@ -11,7 +11,7 @@ echo
 echo "Started setup.sh for provisioning this node"
 
 # Check for patch updates - slows up boot so need a way of avoiding this
-#yum update -y --disableplugin=fastestmirror
+yum update -y --disableplugin=fastestmirror
 
 # Need accurate time
 yum -y install ntp
@@ -23,8 +23,9 @@ systemctl start ntpd
 echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | tee /etc/yum.repos.d/MariaDB.repo
 
 
-# Need just the client - 10.4
-yum -y install MariaDB-client
+# MariaDB 10.4 Python 3.6
+
+yum -y install MariaDB-server MariaDB-client MariaDB-devel python3
 
 # InfluxDB
 #cat <<EOF | sudo tee /etc/yum.repos.d/influxdb.repo
@@ -42,40 +43,34 @@ yum -y install MariaDB-client
 
 #https://www.fosslinux.com/8328/how-to-install-and-configure-grafana-on-centos-7.htm
 # Grafana v7.x
-#cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
-#[grafana]
-#name=grafana
-#baseurl=https://packages.grafana.com/oss/rpm
-#repo_gpgcheck=1
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://packages.grafana.com/gpg.key
-#sslverify=1
-#sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-#EOF
-#yum -y install grafana
-#mkdir -p /etc/grafana
-#cp /vagrant/grafana.ini /etc/grafana/grafana.ini
+cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
+[grafana]
+name=grafana
+baseurl=https://packages.grafana.com/oss/rpm
+repo_gpgcheck=1
+enabled=1
+gpgcheck=1
+gpgkey=https://packages.grafana.com/gpg.key
+sslverify=1
+sslcacert=/etc/pki/tls/certs/ca-bundle.crt
+EOF
+yum -y install grafana
+mkdir -p /etc/grafana
+cp /vagrant/config/grafana.ini /etc/grafana/grafana.ini
 
-# Telegraf
-#wget https://dl.influxdata.com/telegraf/releases/telegraf-1.15.3-1.x86_64.rpm
-#yum -y localinstall telegraf-1.15.3-1.x86_64.rpm
-#mkdir -p /etc/telegraf
-#cp /vagrant/telegraf.conf /etc/telegraf/telegraf.conf
+echo "Starting MariaDB..."
+systemctl enable mariadb
+systemctl start mariadb
 
-#echo "Starting InfluxDB..."
-#systemctl enable influxdb
-#systemctl start influxdb
-#sleep 10
-##curl -XPOST "http://localhost:8086/query" --data-urlencode "q=CREATE USER monitor WITH PASSWORD 'secretsql' WITH ALL PRIVILEGES"
+echo "Secure MariaDB..."
+cp /vagrant/scripts/secure_mysql.sh /tmp/secure_mysql.sh
+chmod +x /tmp/secure_mysql.sh
+cd /tmp
+./secure_my_sql.sh "tradrthemoon"
 
-#echo "Starting Telegraf..."
-#systemctl enable telegraf
-#systemctl start telegraf
-
-#echo "Starting Grafana..."
-#systemctl enable grafana-server
-#systemctl start grafana-server
+echo "Starting Grafana..."
+systemctl enable grafana-server
+systemctl start grafana-server
 ##systemctl status grafana-server
 
 echo "Finished setup.sh OK for provisioning this node"
