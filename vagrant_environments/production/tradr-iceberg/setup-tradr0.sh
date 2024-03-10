b3ceab0cbe5bb8b03b9cf659aca83669adf9c89a
crouchr <richard.crouch100@gmail.com>
2024-03-10 17:29:37 +0000

    Test it brings up one instance

--- vagrant_environments/production/tradr-iceberg/setup-tradr0.sh
+++ vagrant_environments/production/tradr-iceberg/setup-tradr0.sh
@@ -13,14 +13,6 @@ echo "Started setup.sh for provisioning this node"
 # Check for patch updates - slows up boot so need a way of avoiding this
 yum update -y --disableplugin=fastestmirror
 
-# Need accurate time
-#yum -y install ntp
-#echo "Starting NTPd..."
-#systemctl enable ntpd
-#systemctl start ntpd
-
-# MOTD
-# cp /vagrant/motd/motd /etc/motd
 
 # Allow to run X over SSH - add this to the packer file
 # cp /vagrant/config/sshd_config /etc/ssh/sshd_config
@@ -30,7 +22,6 @@ yum update -y --disableplugin=fastestmirror
 # yum install mpg123
 
 # Apache and PHP for web front end
-
 # TRADR web front-end - move into github so can be updated
 # mkdir -p /var/www/html/tradr/
 # cp /vagrant/apache/test.html /var/www/html/tradr/test.html
@@ -39,53 +30,19 @@ yum update -y --disableplugin=fastestmirror
 # cp /vagrant/apache/success.php /var/www/html/tradr/success.php
 
 # Web Infrastructure code
-#cp /vagrant/apache/bootstrap.min.css /var/www/html/tradr/bootstrap.min.css
-#cp /vagrant/apache/jquery.min.js /var/www/html/tradr/jquery.min.js
-#cp /vagrant/apache/tradr-webui.css /var/www/html/tradr/tradr-webui.css
-#chown -R apache:apache /var/www/html/tradr/
-#chmod -R 755 /var/www/html/tradr/
-#echo "Starting httpd..."
-#systemctl enable httpd.service
-#systemctl start httpd.service
-
-
-
-# MariaDB 10.4
-#echo -e "[mariadb]\nname=MariaDB Repository\nbaseurl=http://yum.mariadb.org/10.4/centos7-amd64\ngpgcheck=1\ngpgkey=https://yum.mariadb.org/RPM-GPG-KEY-MariaDB" | tee /etc/yum.repos.d/MariaDB.repo
-#yum -y install MariaDB-server MariaDB-client MariaDB-devel
-
-#https://www.fosslinux.com/8328/how-to-install-and-configure-grafana-on-centos-7.htm
-# Grafana v10.x
-#cat <<EOF | sudo tee /etc/yum.repos.d/grafana.repo
-#[grafana]
-#name=grafana
-#baseurl=https://packages.grafana.com/oss/rpm
-#repo_gpgcheck=1
-#enabled=1
-#gpgcheck=1
-#gpgkey=https://packages.grafana.com/gpg.key
-#sslverify=1
-#sslcacert=/etc/pki/tls/certs/ca-bundle.crt
-#EOF
-#yum -y install grafana
-#mkdir -p /etc/grafana
-#cp /vagrant/config/grafana.ini /etc/grafana/grafana.ini
-
-#echo "Starting MariaDB..."
-#systemctl enable mariadb
-#systemctl start mariadb
+# cp /vagrant/apache/bootstrap.min.css /var/www/html/tradr/bootstrap.min.css
+# cp /vagrant/apache/jquery.min.js /var/www/html/tradr/jquery.min.js
+# cp /vagrant/apache/tradr-webui.css /var/www/html/tradr/tradr-webui.css
+# chown -R apache:apache /var/www/html/tradr/
+# chmod -R 755 /var/www/html/tradr/
 
 # FIXME : just leave it insecure for now
-##echo "Secure MariaDB..."
-##cp /vagrant/scripts/secure_mysql.sh /tmp/secure_mysql.sh
-##chmod +x /tmp/secure_mysql.sh
-##cd /tmp
-##./tmp/secure_my_sql.sh "degenerates"
+## echo "Secure MariaDB..."
+## cp /vagrant/scripts/secure_mysql.sh /tmp/secure_mysql.sh
+## chmod +x /tmp/secure_mysql.sh
+## cd /tmp
+## ./tmp/secure_my_sql.sh "degenerates"
 
-#echo "Starting Grafana..."
-#systemctl enable grafana-server
-#systemctl start grafana-server
-##systemctl status grafana-server
 
 echo "Finished setup.sh OK for provisioning this node"
 # echo "PHP Test Page : http://127.0.0.1:7080/php.ini"
