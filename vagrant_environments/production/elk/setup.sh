1947d9474748309a5efb8f4ff381858a97199620
crouchr <richard.crouch@protonmail.com>
2024-04-07 23:14:12 +0100

    bug fix : logstash.conf

--- vagrant_environments/production/elk/setup.sh
+++ vagrant_environments/production/elk/setup.sh
@@ -7,8 +7,16 @@ echo
 echo "Started setup.sh for provisioning this ELK 7.9.2 (OSS) node"
 
 yum update -y --disableplugin=fastestmirror
-yum -y install java-1.8.0-openjdk.x86_64
+
+echo 'Installing Java 11 JDK...'
+# yum -y install java-1.8.0-openjdk.x86_64
+yum -y install java-11-openjdk-devel
+
 # cp /vagrant/config.yml /etc/docker-distribution/registry/config.yml
+
+mkdir -p /etc/elasticsearch
+mkdir -p /etc/logstash/conf.d
+mkdir -p /etc/kibana
 
 # ELK
 #wget -O /tmp/elasticsearch-7.9.2-x86_64.rpm https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
@@ -21,22 +29,18 @@ wget -O /tmp/kibana-oss-7.9.2-x86_64.rpm http://192.168.1.4/centos7-packages/kib
 
 echo 'Copy packages...'
 #cp /vagrant/packages/*.rpm /tmp/
-sudo yum -y localinstall /tmp/elasticsearch-oss-7.9.2-x86_64.rp
-sudo yum -y localinstall /tmp/logstash-oss-7.9.2.rpm
-sudo yum -y localinstall /tmp/kibana-oss-7.9.2-x86_64.rpm
+yum -y localinstall /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
+yum -y localinstall /tmp/logstash-oss-7.9.2.rpm
+yum -y localinstall /tmp/kibana-oss-7.9.2-x86_64.rpm
 
 # rpm -ivh /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
 # rpm -ivh /tmp/kibana-oss-7.9.2-x86_64.rpm
 
-sudo mkdir -p /etc/elasticsearch
-sudo mkdir -p /etc/logstash/conf.d
-sudo mkdir -p /etc/kibana
-
 echo 'Copy configuration files...'
 cp /vagrant/config/ip_to_honeypot_name_mapping.csv /tmp/
-cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/
-cp /vagrant/config/logstash.conf /etc/logstash/conf.d/
-cp /vagrant/config/kibana.yml /etc/kibana/
+cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
+cp /vagrant/config/logstash.conf /etc/logstash/conf.d/logstash.conf
+cp /vagrant/config/kibana.yml /etc/kibana/kibana.yml
 
 # Telegraf
 #wget https://dl.influxdata.com/telegraf/releases/telegraf-1.8.3-1.x86_64.rpm
@@ -44,9 +48,10 @@ cp /vagrant/config/kibana.yml /etc/kibana/
 #cp /vagrant/telegraf.conf /etc/telegraf/telegraf.conf
 
 echo "Enable ELK services..."
-sudo systemctl enable logstash.service
-sudo systemctl enable elasticsearch.service
-sudo systemctl enable kibana.service
+systemctl daemon-reload
+systemctl enable elasticsearch.service
+systemctl enable logstash.service
+systemctl enable kibana.service
 
 echo "Start ELK services..."
 sudo systemctl start elasticsearch.service
