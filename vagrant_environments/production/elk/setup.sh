eadb790cb7f39860aaf12f202119c3ba6eec97f5
crouchr <richard.crouch@protonmail.com>
2024-04-08 19:57:23 +0100

    bug fix : logstash.conf - use ossec json as input source

--- vagrant_environments/production/elk/setup.sh
+++ vagrant_environments/production/elk/setup.sh
@@ -9,19 +9,11 @@ echo "Started setup.sh for provisioning this ELK 7.9.2 (OSS) node"
 yum update -y --disableplugin=fastestmirror
 
 echo 'Installing Java 11 JDK...'
-# yum -y install java-1.8.0-openjdk.x86_64
 yum -y install java-11-openjdk-devel
-
-# cp /vagrant/config.yml /etc/docker-distribution/registry/config.yml
 
 mkdir -p /etc/elasticsearch
 mkdir -p /etc/logstash/conf.d
 mkdir -p /etc/kibana
-
-# ELK
-#wget -O /tmp/elasticsearch-7.9.2-x86_64.rpm https://artifacts.elastic.co/downloads/elasticsearch/elasticsearch-7.9.2-x86_64.rpm
-#rpm -ivh /tmp/elasticsearch-7.9.2-x86_64.rpm
-#yum -y localinstall elasticsearch-7.9.2-x86_64.rpm
 
 wget -O /tmp/elasticsearch-oss-7.9.2-x86_64.rpm http://192.168.1.4/centos7-packages/elasticsearch-oss-7.9.2-x86_64.rpm
 wget -O /tmp/logstash-oss-7.9.2.rpm http://192.168.1.4/centos7-packages/logstash-oss-7.9.2.rpm
@@ -33,19 +25,11 @@ yum -y localinstall /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
 yum -y localinstall /tmp/logstash-oss-7.9.2.rpm
 yum -y localinstall /tmp/kibana-oss-7.9.2-x86_64.rpm
 
-# rpm -ivh /tmp/elasticsearch-oss-7.9.2-x86_64.rpm
-# rpm -ivh /tmp/kibana-oss-7.9.2-x86_64.rpm
-
 echo 'Copy configuration files...'
 cp /vagrant/config/ip_to_honeypot_name_mapping.csv /tmp/
 cp /vagrant/config/elasticsearch.yml /etc/elasticsearch/elasticsearch.yml
-cp /vagrant/config/logstash.conf /etc/logstash/conf.d/logstash.conf
+cp /vagrant/config/ossec.conf /etc/logstash/conf.d/ossec.conf
 cp /vagrant/config/kibana.yml /etc/kibana/kibana.yml
-
-# Telegraf
-#wget https://dl.influxdata.com/telegraf/releases/telegraf-1.8.3-1.x86_64.rpm
-#yum -y localinstall telegraf-1.8.3-1.x86_64.rpm
-#cp /vagrant/telegraf.conf /etc/telegraf/telegraf.conf
 
 echo "Enable ELK services..."
 systemctl daemon-reload
